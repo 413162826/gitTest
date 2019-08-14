f5e4d7200ac7cd01cf4c3e9573a6d94822200efd
suguowen <suguowen@cvte.com>
2019-08-15 00:10:49 +0800

    fix bugfix002*2

--- src/main/java/com/suguowen/gittest/demo/NewFile.java
+++ src/main/java/com/suguowen/gittest/demo/NewFile.java
@@ -3,6 +3,7 @@ package com.suguowen.gittest.demo;
 public class NewFile {
     public static void main(String[] args) {
         System.out.println("bugfix002");
+        System.out.println("bugfix002");
     }
 
     public void test1(){
