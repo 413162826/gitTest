c057cf2ec3c96dfe2732bd72ec1c43be0a5c4292
suguowen <suguowen@cvte.com>
2019-08-14 14:49:38 +0800

    bugfix 提交test和main

--- src/main/java/com/suguowen/gittest/demo/NewFile.java
+++ src/main/java/com/suguowen/gittest/demo/NewFile.java
@@ -1,4 +1,11 @@
 package com.suguowen.gittest.demo;
 
 public class NewFile {
+    public static void main(String[] args) {
+
+    }
+
+    public void test1(){
+        System.out.println("create by bugfix001");
+    }
 }
